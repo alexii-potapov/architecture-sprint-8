63d2313e459f2b5e2f8da1f5f1a1b17d3e719a1a
Потапов Алексей <potapov@anc.space>
2025-05-02 19:41:07 +0300

    Обновления Задания

--- Reports/Controllers/ReportsController.cs
+++ Reports/Controllers/ReportsController.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,18 +15,32 @@ public class ReportsController : ControllerBase
     ];
 
     [HttpGet]
-    public IEnumerable<Report> Get()
+    public IActionResult Get()
     {
         // Получаем claims пользователя из токена
-        string? userId = User.FindFirst("sub")?.Value;
-        string? userName = User.FindFirst("preferred_username")?.Value;
-        System.Console.WriteLine($"Request from UserId = {userId}, Name = {userName}");
+        string? userId = User.FindFirst("preferred_username")?.Value;
+        string? userName = User.FindFirst("name")?.Value;
+        string? userRoles = User.FindFirst("realm_access")?.Value;
 
-        return Enumerable.Range(1, 5).Select(index => new Report
+        System.Console.WriteLine($"Request from UserId = {userId}, Name = {userName}");
+        
+        RealmAccess? realmAccess = JsonSerializer.Deserialize<RealmAccess>(userRoles);
+        List<string> roles = realmAccess?.Roles ?? new List<string>();
+       
+        if (!roles.Contains("prothetic_user"))
+        {   
+            System.Console.WriteLine("Not prothetic_user");
+
+            return Unauthorized();
+        }
+        
+        Report[] report = Enumerable.Range(1, 5).Select(index => new Report
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(-index)),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        
+        return Ok(report);
     }
 }
